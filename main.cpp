fb5819cfe1d107a1fcdbfb1ac14cf309423ee869
Lili <charlyrsmail@gmail.com>
2020-10-08 21:37:35 +0300

    task A

--- main.cpp
+++ main.cpp
@@ -1,39 +1,20 @@
 #include <iostream>
 #include <fstream>
-
-
-int main() {
-    std::ifstream fin("sample.txt");
-    char str[300];
-    int dates = 0;
-    int day = 0;
-    int month = 0;
-    if(fin.is_open()) {
-        while (fin >> str) {
-            if (std::strlen(str) == 10 && str[2] == '/' && str[5] == '/') {
-                if (str[0] <= '9' && str[0] >= '0' && str[1] <= '9' && str[1] >= '0'
-                    && str[3] <= '9' && str[3] >= '0' && str[4] <= '9' && str[4] >= '0'
-                    && str[6] <= '9' && str[6] >= '0' && str[7] <= '9' && str[7] >= '0'
-                    && str[8] <= '9' && str[8] >= '0' && str[9] <= '9' && str[9] >= '0') {
-
-                    day = (str[0] - '0') * 10 + (str[1] - '0');
-                    month = (str[3] - '0') * 10 + (str[4] - '0');
-
-                    if ((month == 4 || month == 6 || month == 9 || month == 11) && day <= 30)
-                        dates++;
-                    if ((month == 1 || month == 3 || month == 5 || month == 7 ||
-                         month == 8 || month == 10 || month == 12) && day <= 31)
-                        dates++;
-                    if (month == 2 && day <= 28)
-                        dates++;
-                }
-            }
-
-        }
+char *analogstrchr(const char *str, char symbol) {
+    while (*str != symbol) {
+        if (!*str++) return nullptr;
     }
-    else std::cout << "File is not opened\n";
+    return (char *)str;
+}
+int main() {
+    std::fstream fin ("function.txt");
+    char str[300];
+    fin >> str;
+    char symbol;
+    std::cout<<"Enter symbol\n";
+    std :: cin >> symbol;
+    std::cout << analogstrchr(str, symbol);
     fin.close();
-    std::cout << "Found " << dates << " correct dates";
 
     return 0;
 }

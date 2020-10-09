4ddfa1602858084357a89cf5984d0f12a6216d21
Lili <charlyrsmail@gmail.com>
2020-10-09 15:48:57 +0300

    works for each string

--- taskB.cpp
+++ taskB.cpp
@@ -10,32 +10,56 @@ int main() {
     int month = 0;
     int year = 0;
     if (fin.is_open()) {
-        while (fin >> str) {
-            if (std::strlen(str) == 10 && str[2] == '/' && str[5] == '/') {
-                if (str[0] <= '9' && str[0] >= '0' && str[1] <= '9' && str[1] >= '0'
-                    && str[3] <= '9' && str[3] >= '0' && str[4] <= '9' && str[4] >= '0'
-                    && str[6] <= '9' && str[6] >= '0' && str[7] <= '9' && str[7] >= '0'
-                    && str[8] <= '9' && str[8] >= '0' && str[9] <= '9' && str[9] >= '0') {
+        while (fin.getline(str, 300)) {
+            dates = 0;
+            char word[300];
+            int l = std::strlen(str);
+            str[l] = ' ';
+            str[l + 1] = '\0';
+            l++;
+            int index = 0;
+            for (int i = 0; i < l; ++i) {
+                if (str[i] == ' ') {
+                    word[index] = '\0';
+                    if (index > 0) {
+                        index = 0;
+                    }
+                    continue;
+                }
 
-                    day = (str[0] - '0') * 10 + (str[1] - '0');
-                    month = (str[3] - '0') * 10 + (str[4] - '0');
-                    year = (str[6] - '0') * 1000 + (str[7] - '0') * 100 + (str[8] - '0') * 10 + (str[9] - '0');
+                word[index] = str[i];
+                index++;
+                if (std::strlen(word) == 10 && word[2] == '/' && word[5] == '/') {
+                    if (word[0] <= '9' && word[0] >= '0' && word[1] <= '9' && word[1] >= '0'
+                        && word[3] <= '9' && word[3] >= '0' && word[4] <= '9' && word[4] >= '0'
+                        && word[6] <= '9' && word[6] >= '0' && word[7] <= '9' && word[7] >= '0'
+                        && word[8] <= '9' && word[8] >= '0' && word[9] <= '9' && word[9] >= '0') {
 
-                    if ((month == 4 || month == 6 || month == 9 || month == 11) && day <= 30)
-                        dates++;
-                    if ((month == 1 || month == 3 || month == 5 || month == 7 ||
-                        month == 8 || month == 10 || month == 12) && day <= 31)
-                        dates++;
-                    if ((month == 2 && day <= 28) || (month == 2 && year % 4 == 0 && day <= 29))
-                        dates++;
+                        day = (word[0] - '0') * 10 + (word[1] - '0');
+                        month = (word[3] - '0') * 10 + (word[4] - '0');
+                        year = (word[6] - '0') * 1000 + (word[7] - '0') * 100 + (word[8] - '0') * 10 + (word[9] - '0');
+
+                        if ((month == 4 || month == 6 || month == 9 || month == 11) && day <= 30)
+                            dates++;
+                        if ((month == 1 || month == 3 || month == 5 || month == 7 ||
+                            month == 8 || month == 10 || month == 12) && day <= 31)
+                            dates++;
+                        if ((month == 2 && day <= 28) || (month == 2 && year % 4 == 0 && day <= 29))
+                            dates++;
+                        for (int k = 0; k < 300; ++k) {
+                            word[k] = '\0';
+                        }
+                    }
                 }
             }
 
+
+            std::cout << "Found " << dates << " correct dates" << std::endl;
         }
     }
     else std::cout << "File is not opened\n";
     fin.close();
-    std::cout << "Found " << dates << " correct dates";
+
 
     return 0;
 }

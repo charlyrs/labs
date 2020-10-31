7c72e21b0979f257b7d469800949ccf9ae4ce189
Lili <charlyrsmail@gmail.com>
2020-10-31 17:12:35 +0300

    normal main()

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include "Fraction.h"
 int main() {
-    int a;
     Fraction b(15, 90);
-    Fraction n (1, -2);
-    Fraction c (2, 3);
-    Fraction v (2, 3);
+    Fraction n(2, -1);
+    Fraction c(2, 3);
+    Fraction v(2, 3);
     b.MultiplyRational(n);
     b.Print();
-    c.DivideRational(v);
+    c.AddRational(v);
     c.Print();
+    std::cout << c.GetNumerator();
     
 
 

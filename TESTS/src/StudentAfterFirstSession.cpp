06042aa351409f943253abd63f63432680d6f1da
Lili <charlyrsmail@gmail.com>
2020-11-27 19:52:22 +0300

    TESTS

---
+++ TESTS/src/StudentAfterFirstSession.cpp
@@ -0,0 +1,57 @@
+#include "StudentAfterFirstSession.h"
+#include <iostream>
+
+StudentAfterFirstSession::StudentAfterFirstSession(char *_name, int _course, int _group, int* _marks)
+        :Student(_name,_course, _group){
+    session = 1;
+    for (int i=0; i<4; ++i){
+        marksAfterFirstSession[i]=_marks[i];
+    }
+
+}
+StudentAfterFirstSession::StudentAfterFirstSession(const Student& student, int* _marks)
+        :Student(student){
+    session = 1;
+    for (int i=0; i<4; ++i){
+        marksAfterFirstSession[i]=_marks[i];
+    }
+
+}
+StudentAfterFirstSession::StudentAfterFirstSession(const StudentAfterFirstSession& a) : Student(a) {
+    session = 1;
+    for (int i=0; i<4; ++i){
+        marksAfterFirstSession[i]=a.marksAfterFirstSession[i];
+    }
+
+}
+
+
+void StudentAfterFirstSession ::SetFirstSessionMarks(int *_marks) {
+
+    for (int k=0; k<4;++k){
+        marksAfterFirstSession[k]=_marks[k];
+    }
+
+}
+int* StudentAfterFirstSession ::GetFirstSessionMarks() {
+    return marksAfterFirstSession;
+}
+double StudentAfterFirstSession::AverageScore() {
+    double sum=0;
+    for (int i=0; i<4; ++i){
+        sum+=marksAfterFirstSession[i];
+    }
+    return sum/4.0;
+}
+std::ostream& operator << (std::ostream &out, StudentAfterFirstSession& a){
+    out << "Name: " << a.GetName() << "  ";
+    out << "Course: " << a.GetCourse() << "  ";
+    out << "Group: " << a.GetGroup() << "  ";
+    out << "Marks after first session are ";
+    for (int i=0; i<4; ++i){
+        out << a.GetFirstSessionMarks()[i] << " ";
+    }
+
+    return out;
+
+}
---
+++ TESTS/src/StudentAfterSecondSession.cpp
@@ -0,0 +1,64 @@
+#include "StudentAfterSecondSession.h"
+#include <iostream>
+StudentAfterSecondSession::StudentAfterSecondSession(char *name, int course, int group, int marks1[4], int* marks2)
+        :StudentAfterFirstSession(name, course, group, marks1){
+    session = 2;
+    for (int i=0; i<5; ++i){
+        marksAfterSecondSession[i]=marks2[i];
+    }
+
+
+}
+StudentAfterSecondSession::StudentAfterSecondSession(const StudentAfterFirstSession & stud, int* marks2)
+        :StudentAfterFirstSession(stud){
+    session = 2;
+    for (int i=0; i<5; ++i){
+        marksAfterSecondSession[i]=marks2[i];
+    }
+
+}
+StudentAfterSecondSession::StudentAfterSecondSession(const StudentAfterSecondSession&a) : StudentAfterFirstSession(a) {
+    session = 2;
+    for (int i=0; i<5; ++i){
+        marksAfterSecondSession[i]=a.marksAfterSecondSession[i];
+    }
+
+
+}
+
+void StudentAfterSecondSession::SetSecondSessionMarks(int *m) {
+
+    for (int i=0; i<5; ++i){
+        marksAfterSecondSession[i]=m[i];
+    }
+
+
+}
+int* StudentAfterSecondSession::GetSecondSessionMarks() {
+    return marksAfterSecondSession;
+}
+double StudentAfterSecondSession::AverageScore() {
+    double sum=0;
+    for ( int i=0; i <4; ++i){
+        sum+= GetFirstSessionMarks()[i];
+    }
+    for (int i=0; i<5; ++i){
+        sum+=marksAfterSecondSession[i];
+    }
+    return sum/9.0;
+}
+std::ostream& operator << (std::ostream &out, StudentAfterSecondSession& a){
+    out << "Name: " << a.GetName() << "  ";
+    out << "Course: " << a.GetCourse() << "  ";
+    out << "Group: " << a.GetGroup() << "  ";
+
+    out << "Marks after the first session are ";
+    for (int i=0; i<4; ++i){
+        out << a.GetFirstSessionMarks()[i] << " ";
+    }
+    out << "\nMarks after the second session are ";
+    for (int i=0; i<5; ++i){
+        out << a.marksAfterSecondSession[i] << " ";
+    }
+    return out;
+}
